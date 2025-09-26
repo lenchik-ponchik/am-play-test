c9664ef4e8ed5eed4c6ebcf9d0ae4ee97e8cc046
Leonid Chedrik <chedrik.leonid@gmail.com>
2025-09-26 19:37:11 +0300

    s

---
+++ src/routes/+page.svelte
@@ -0,0 +1,50 @@
+<main class="min-h-screen">
+	<div class="mx-auto">
+		<div class="grid grid-cols-1 md:grid-cols-2">
+			<div class="outline-1 outline-dashed p-4">
+				<div style="position:relative;aspect-ratio:16/9;">
+					<iframe
+						loading="lazy"
+						src="https://player.mux.com/rR8P8mSaKDzz02TsftugTUdI00cQPJX00oy"
+						title="Mux video player"
+						style="border:none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;"
+						allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
+						allowfullscreen
+					>
+					</iframe>
+				</div>
+			</div>
+
+			<div class="outline-1 outline-dashed p-4">
+				<div style="position:relative;aspect-ratio:16/9;">
+					<div style="position:relative;aspect-ratio:16/9;">
+						<iframe
+							loading="lazy"
+							title="Gumlet video player"
+							src="https://play.gumlet.io/embed/68d6bde0dd32d42019fde4b4"
+							style="border:none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;"
+							allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture; fullscreen;"
+						>
+						</iframe>
+					</div>
+				</div>
+			</div>
+
+			<div class="outline-1 outline-dashed p-4">
+				<div style="position: relative; padding-top: 56.25%; width: 100%">
+					<iframe
+						loading="lazy"
+						title="KINESCOPE video player"
+						src="https://kinescope.io/embed/j9HpZi954pWA2DG8c3z93L"
+						allow="autoplay; fullscreen; picture-in-picture; encrypted-media; gyroscope; accelerometer; clipboard-write; screen-wake-lock;"
+						frameborder="0"
+						allowfullscreen
+						style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;"
+					></iframe>
+				</div>
+			</div>
+
+			<div class="outline-1 outline-dashed p-4"></div>
+		</div>
+	</div>
+</main>
